355f0d959f7f3a426750e2fef173eae1157f3396
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-11-23 21:33:45 +0530

    feat : installed env and stored db uri

--- harvestly-backend/db.js
+++ harvestly-backend/db.js
@@ -1,6 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://harvestly:Harvestly12%23@harvestly.lkglqyc.mongodb.net/?retryWrites=true&w=majority";
+require("dotenv").config()
+const uri = process.env.DB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
