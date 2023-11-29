ec84379b3773503eb561a96c7e39e84b3a26d78b
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-11-29 17:37:32 +0530

    fix : server.js

--- harvestly-backend/server.js
+++ harvestly-backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express'
 const app = express()
+const PORT = 4000
 
 app.get("/api", (req, res)=>{
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
 
-app.listen(8000, ()=>{
-    console.log("Server started at 8000");
+app.get("/", (req, res)=>{
+    res.json({"home": "Welcome to the home page"})
 })
+
+app.listen(PORT, ()=>{
+    console.log("Server started at 4000");
+})
+
+module.exports = app
