f0971e88c163d92fbeb5683ab917c5129c2ffe13
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2024-02-15 18:23:12 +0530

    feat : added payment gateway

--- routes/payment.js
+++ routes/payment.js
@@ -2,9 +2,7 @@ import express from "express";
 const router = express.Router();
 import * as paymentController from "../controllers/paymentController.js";
 
-// Google SSO routes
-router.get("/order", paymentController.createOrder);
-
-router.get("/verify", paymentController.verifyOrder);
+router.post("/order", paymentController.createOrder);
+router.post("/verify", paymentController.verifyOrder);
 
 export default router;
